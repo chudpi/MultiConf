05f091981c8b1ca7938c253c7bec06e79409c2c7
chudpi <p@chudpi.org>
2009-08-23 22:52:48 -0700

    MultiConf.pm: initial POD comments t/ : changed the shebang headding to one I like more.
    There are no actual code changes here, this is more for a playing with git than anything else, since I'm new to it.

--- MultiConf/lib/MultiConf.pm
+++ MultiConf/lib/MultiConf.pm
@@ -3,50 +3,54 @@ package MultiConf;
 use warnings;
 use strict;
 
+use version; our $VERSION = '0.01';
+
+1; ## End of MultiConf package
+
+
 =head1 NAME
 
-MultiConf - The great new MultiConf!
+MultiConf - Multi-level, multi-source configuration system
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+ #code examples TBD here
 
-Perhaps a little code snippet.
+=head1 DESCRIPTION
 
-    use MultiConf;
+Upon instancitation, the module loads configuration in a cascading style
+from a variety of sources using Config::Any, and multiple levels.  
+The typical levels are:
 
-    my $foo = MultiConf->new();
-    ...
+=over 4
+
+global - typically default configuration typically in a directory
+located located within the working copy and checked into the repository
+as part of the release bundle.  Uses L<Config::Any>.
+
+local - configuration specific to the environment to which the software is
+released and on which it runs.  This typically exists outside of the working
+copy to preserve developers from controling the integration ( important if 
+you are developing for a publicly traded organization (US) ).  Uses L<Config::Any>
+
+command line - the options specified on the commannd line.  Uses L<Getopt::Long>.
+
+=back
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Initially MultiConf will be developed with a pure OO philosophy.  Ergo, no exports.
 
 =head1 FUNCTIONS
 
-=head2 function1
+=head2 load
 
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+Constructor.
+XXX Write it up.
 
 =head1 AUTHOR
 
@@ -54,12 +58,9 @@ Pete Chudykowski, C<< <p at chudpi.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-multiconf at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MultiConf>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to C<bug-multiconf at rt.cpan.org>, 
+or through the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MultiConf>.  
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
@@ -106,5 +107,3 @@ See http://dev.perl.org/licenses/ for more information.
 
 
 =cut
-
-1; # End of MultiConf
